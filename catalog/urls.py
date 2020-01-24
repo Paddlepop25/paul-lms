27ba506e22c023a8fb71947c340dea076c6dab2c
Ubuntu <ubuntu@ip-172-31-88-25.ec2.internal>
2020-01-24 06:24:03 +0000

    CREATE function for lms

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import show_courses
+from .views import show_courses, create_course
 
 urlpatterns = [
-    path('', show_courses)
+    path('', show_courses),
+    path('create', create_course)
    ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Course
+from .forms import CourseForm
 
 # Create your views here.
 def show_courses(request):
@@ -8,3 +9,17 @@ def show_courses(request):
         'all_courses': all_courses
     })
     
+def create_course(request):
+    if request.method == 'POST':
+        create_course_form = CourseForm(request.POST)
+        if create_course_form.is_valid():
+            create_course_form.save()
+            return redirect(reverse(show_courses))
+
+    else:
+        create_course_form = CourseForm()
+
+    return render(request, 'catalog/create_course.template.html', {
+        'form':create_course_form
+    })
+        
