45323cff675afa32cc0394e89c2cbc824a9b58b9
Ubuntu <ubuntu@ip-172-31-88-25.ec2.internal>
2020-01-26 03:57:22 +0000

    UPDATE function for lms

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import show_courses, create_course
+from .views import show_courses, create_course, update_course
 
 urlpatterns = [
-    path('', show_courses),
-    path('create', create_course)
+    path('', show_courses, name='show_courses'),
+    path('create', create_course),
+    path('update/<course_id>', update_course, name='update_course')
    ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Course
 from .forms import CourseForm
 
@@ -22,4 +22,22 @@ def create_course(request):
     return render(request, 'catalog/create_course.template.html', {
         'form':create_course_form
     })
+    
+def update_course(request, course_id):
+    course_being_updated = get_object_or_404(Course, pk=course_id)
+    
+    if request.method == 'POST':
+        update_course_form = CourseForm(request.POST, instance=course_being_updated)
+        if update_course_form.is_valid():
+            update_course_form.save()
+            
+        # always make sure to return the redirect
+        return redirect(reverse(show_courses))
+    
+    else:
+        update_course_form = CourseForm(instance=course_being_updated)
+        
+    return render(request, 'catalog/update_course.template.html', {
+        'form':update_course_form
+    })    
         
